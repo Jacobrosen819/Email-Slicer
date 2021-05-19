b5f7c229d333921071b3880316731ae95842ee2b
Jacob Rosen <jacobrosen819@gmail.com>
2021-05-19 11:22:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+email_input = input("What is the email? ")
+def email_slice (email_input):
+  first_name_slice = email_input.split("@", 1)
+  first_name = first_name_slice[0]
+  retunr
+
+email_slice()
