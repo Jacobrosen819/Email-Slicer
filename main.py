e1638ea28322858ad5c56f8368d20bd142faad5e
viper2222 <>
2021-05-27 00:48:22 +0000

    It actually works

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ email_input = input("What is the email? ")
 def email_slice (email_input):
   first_name_slice = email_input.split("@", 1)
   first_name = first_name_slice[0]
-  retunr
+  company_list = first_name_slice[1]
+  company_splice = company_list.split(".", 1)
+  company = company_splice[0]
+  print("\n")
+  print ("First Name: " + first_name)
+  print("Company: " + company)
 
-email_slice()
+email_slice(email_input)
